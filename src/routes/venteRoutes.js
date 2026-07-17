6764ffd47b51f87e33d5a5f3cab6404e5f084799
Phil <allabyo04@gmail.com>
2026-07-17 12:57:50 +0100

    Correction fichier venteRoutes.js non mis a jour

--- src/routes/venteRoutes.js
+++ src/routes/venteRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { creerVente, annulerVente, listerVentes } = require('../controllers/venteController');
+const {
+  creerVente, annulerVente, listerVentes,
+  demanderAnnulation, listerDemandesAnnulation, rejeterAnnulation,
+} = require('../controllers/venteController');
 const { requireAuth, requireRole } = require('../middleware/auth');
 
 router.get('/', requireAuth, listerVentes);
+router.get('/demandes-annulation', requireAuth, requireRole('ADMIN'), listerDemandesAnnulation);
 router.post('/', requireAuth, creerVente);
+router.post('/:id/demander-annulation', requireAuth, demanderAnnulation);
+router.post('/:id/rejeter-annulation', requireAuth, requireRole('ADMIN'), rejeterAnnulation);
 router.post('/:id/annuler', requireAuth, requireRole('ADMIN'), annulerVente);
 
 module.exports = router;
